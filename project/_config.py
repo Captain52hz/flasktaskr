ae9983655cec3aa39b1085594c7f30709a3b62f7
lxd <kindle_lxd@163.com>
2019-04-08 11:22:19 +0800

    use flaskalchemy

--- project/_config.py
+++ project/_config.py
@@ -6,6 +6,8 @@ DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'myprecious'
+SECRET_KEY = 'my_precious'
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
